0925d1b8e92b89988bc76d464144740b890f22b0
batistaDev1113 <yuniorbatista1113@gmail.com>
2018-09-18 00:34:48 -0400

    deleted unnecessary pictures and compressed the images from img folder using gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,17 @@
 var gulp = require('gulp'),
-    uglify = require('gulp-uglify');
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin');
 
 gulp.task('default', function(){
   gulp.src('js/all.js')
   .pipe(uglify())
   .pipe(gulp.dest('minjs'));
+});
+
+
+//image compress task
+gulp.task('image', function(){
+  gulp.src('img/*')
+  .pipe(imagemin())
+  .pipe(gulp.dest('imgmin'));
 });
