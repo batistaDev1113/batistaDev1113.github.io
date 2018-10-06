76227c0df3f46ee481de3c351b813d5b6772065f
batistaDev1113 <yuniorbatista1113@gmail.com>
2018-10-06 14:50:06 -0400

    path resolved for restaurants.json inside of dbhelper.js

--- projects/mws-restaurant-stage-1/js/dbhelper.js
+++ projects/mws-restaurant-stage-1/js/dbhelper.js
@@ -9,7 +9,8 @@ class DBHelper {
    */
   static get DATABASE_URL() {
     const port = 8000 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    // return `/data/restaurants.json`;
+    return 'https://batistadev1113.github.io/projects/mws-restaurant-stage-1/data/restaurants.json'
   }
 
   /**
