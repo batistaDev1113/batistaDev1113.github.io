3f5ce9ce639701e24e5f30e088da15aaed784d60
batistaDev1113 <yuniorbatista1113@gmail.com>
2018-10-06 15:05:36 -0400

    added new path for pics to be rendered from dbhelper.js

--- projects/mws-restaurant-stage-1/js/dbhelper.js
+++ projects/mws-restaurant-stage-1/js/dbhelper.js
@@ -9,7 +9,6 @@ class DBHelper {
    */
   static get DATABASE_URL() {
     const port = 8000 // Change this to your server port
-    // return `/data/restaurants.json`;
     return 'https://batistadev1113.github.io/projects/mws-restaurant-stage-1/data/restaurants.json'
   }
 
@@ -151,7 +150,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+    return (`https://github.com/batistaDev1113/batistaDev1113.github.io/tree/master/projects/mws-restaurant-stage-1/img${restaurant.photograph}`);
   }
 
   /**
