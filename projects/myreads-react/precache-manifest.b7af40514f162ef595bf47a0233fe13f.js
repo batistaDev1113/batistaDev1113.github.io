329736b65be88c745037a4a627355b8ee3bab36a
batistaDev1113 <yuniorbatista1113@gmail.com>
2018-11-08 20:01:57 -0500

    myreads prject added to portfolio projects

---
+++ projects/myreads-react/precache-manifest.b7af40514f162ef595bf47a0233fe13f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d049b35ab8708874dffd",
+    "url": "/static/js/main.d049b35a.chunk.js"
+  },
+  {
+    "revision": "7ab658190f6ea7fcf217",
+    "url": "/static/js/1.7ab65819.chunk.js"
+  },
+  {
+    "revision": "d049b35ab8708874dffd",
+    "url": "/static/css/main.e6bfaa1d.chunk.css"
+  },
+  {
+    "revision": "9b29b81773e1578de160d1a4e3116145",
+    "url": "/index.html"
+  }
+];
